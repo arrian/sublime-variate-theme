1231e1f8b23aaf3d822c2c9ddc9852bf2c62bd37
Arrian Purcell <arrian.purcell@gmail.com>
2015-03-04 20:11:22 +1100

    Adding get colour from time of day. Adding simulated time of day.

--- Variate.py
+++ Variate.py
@@ -1,13 +1,31 @@
 import sublime, sublime_plugin
 import os
 import random
-import time, threading
+import threading
+
+from datetime import datetime, time
 
 timer_update = None
 
 SUBLIME_PREFERENCES = 'Preferences.sublime-settings'
 
 variate = False
+
+def get_elapsed_seconds():
+	"""
+	Gets the total number of elapsed seconds in the day.
+	"""
+	utcnow = datetime.utcnow()
+	midnight_utc = datetime.combine(utcnow.date(), time(0))
+	delta = utcnow - midnight_utc
+	return delta.total_seconds()
+
+elapsed_seconds_simulated = 0.0
+
+def get_elapsed_seconds_simulated():
+	global elapsed_seconds_simulated
+	elapsed_seconds_simulated = elapsed_seconds_simulated + 1000.0
+	return elapsed_seconds_simulated
 
 class StartVariateCommand(sublime_plugin.WindowCommand):
 
@@ -19,6 +37,7 @@ class StartVariateCommand(sublime_plugin.WindowCommand):
 		self.update()
 
 	def update(self):
+
 		if not variate:
 			print('finished variating')
 			return
@@ -35,7 +54,7 @@ class StartVariateCommand(sublime_plugin.WindowCommand):
 
 		for s in itemlist:
 			if s.childNodes[1].firstChild.nodeValue == 'background':
-				s.childNodes[3].firstChild.replaceWholeText(self.randomColour())
+				s.childNodes[3].firstChild.replaceWholeText(self.get_colour_time_based())
 
 		file_handle = open(settings_path, 'wb')
 		file_handle.write(bytes(xmldoc.toxml(),'UTF-8'))
@@ -46,9 +65,19 @@ class StartVariateCommand(sublime_plugin.WindowCommand):
 		timer_update = threading.Timer(1, self.update)
 		timer_update.start()
 
-	def randomColour(self):
+	def get_colour_random(self):
 		r = lambda: random.randint(0,255)
-		return '#%02X%02X%02X' % (int(r() * 0.5),0,0)
+		return self.get_hex_from_rgb(int(r() * 0.5), 0, 0);
+
+	def get_colour_time_based(self):
+		"""
+		Gets a colour based on the time of the day.
+		"""
+		return self.get_hex_from_rgb(int(255.0 * (get_elapsed_seconds_simulated() / 86400.0)), 0, 0)
+
+	def get_hex_from_rgb(self, r, g, b):
+		return '#%02X%02X%02X' % (r,g,b)
+
 
 class StopVariateCommand(sublime_plugin.WindowCommand):
 
